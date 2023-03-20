b349db480ef789de6a5a6fad8b40a8ba624cec79
EdsonLopesJr <dev_edson@outlook.com>
2023-03-20 15:03:05 -0300

    create Components

---
+++ 2_projeto_vue/src/components/primeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello vue!</h1>
+        <p>Hello! My name is {{ namePerson }}, and I'm a {{ professionPerson }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                namePerson: "Edson",
+                professionPerson: "Developer Front-end and Back-end"
+            }
+        }
+    }
+
+</script>
